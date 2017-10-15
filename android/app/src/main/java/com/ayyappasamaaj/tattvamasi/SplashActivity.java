2a858180ed3b82a8352f42be67ba021a8103fc14
Satya Surya <ssurya@westfield.com>
2017-10-14 18:47:03 -0700

    Android submission changes

--- android/app/src/main/java/com/ayyappasamaaj/tattvamasi/SplashActivity.java
+++ android/app/src/main/java/com/ayyappasamaaj/tattvamasi/SplashActivity.java
@@ -6,6 +6,7 @@ package com.ayyappasamaaj.tattvamasi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -13,8 +14,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
